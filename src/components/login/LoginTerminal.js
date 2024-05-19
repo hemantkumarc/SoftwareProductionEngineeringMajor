d81f134a9ec69da00df6b425fa85db07e14c6104
Hemant Kumar <hemantkumarcpersonal@gmail.com>
2024-05-19 11:06:10 +0530

    Add CourseMap files

---
+++ src/components/login/LoginTerminal.js
@@ -0,0 +1,182 @@
+import React, {useState} from 'react'
+import './terminalStyle.css'
+import ButtonEncrypt from './ButtonEncrypt';
+import styles from "./bubble.module.css";
+
+function LoginTerminal() {
+
+  const [email, setEmail] = useState('');
+  const [showEmail, setShowEmail] = useState(false);
+  const [name, setName] = useState('')
+  const [showName, setShowName] = useState(false)
+  const[password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      setShowEmail(true);
+    }
+  };
+
+  const handleNameKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      setShowName(true);
+    }
+  };
+
+  const handlePasswordKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      setShowPassword(true);
+    }
+  };
+
+  return (
+    <section id='fullScreen'>
+        <div className="terminal-window">
+        <div className="top-bar">
+            <div className="button red"></div>
+            <div className="button yellow"></div>
+            <div className="button green"></div>
+        </div>
+        <div className="content">
+            {/* Your terminal content goes here */}
+            <div id='titleText' className="grid h-screen place-content-center">
+            <BubbleText />
+            </div>
+            <p>Hey there! We're excited to link</p>
+        </div>
+
+        <div id='emailInput' className='row'>
+            <div id='emailPrec' className='col-4'>Could you please provide</div>
+            <div id='emailText' className='col-3'>your email?</div>
+        </div>
+
+            {showEmail ? (
+            <>
+                <div id='inputEmail' className='row'>
+                    <div className='col-1'>
+                        <img className='ticked' src={require('../../assets/tick.png')} />
+                    </div>
+                    <div id='userEmail' className='col-4'>{email}</div>
+                </div>
+
+                <div className='row'>
+                <div className='col-4' id='preName'>Awesome, may I get</div>
+                <div className='col-3' id='userName'>your name?</div>
+                </div>
+                
+                {
+                showName ? (
+                    <>
+                    <div id='inputName' className='row'>
+                        <div className='col-1'>
+                            <img id='tickedName' src={require('../../assets/tick.png')} />
+                        </div>
+                        <div id='nameUser' className='col-4'>{name}</div>
+                    </div>
+                    <div className='row'>
+                    <div className='col-4' id='prePassword'>Finally... please provide a</div>
+                    <div className='col-3' id='passwordLabel'>strong password.</div>
+                    </div>
+
+                    {
+                        showPassword ? (
+                            <div>
+                                <div id='processComplete'>Great. Good to GO!!</div>
+                                <div id='encryptButton'>
+                                    <ButtonEncrypt />
+                                </div>
+                            </div>
+                        ) : 
+                        (
+                            <div id='inputName' className='row'>
+                            <div className='col-1'>   
+                                <img id='arrow' src={require('../../assets/right-arrow-2.png')} />
+                            </div>
+                            <div className='col-1'>
+                                <img id='tildeIcon' src={require('../../assets/tilde-3.png')} />
+                            </div>
+                            <div id='enterPassword' className='col-8'>Enter Password:
+                                <input 
+                                    type="text" 
+                                    id="emailNameField" 
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    onKeyDown={handlePasswordKeyDown}
+                                    autoFocus // Focuses on the input field by default
+                                />
+                            </div>
+                            </div>
+                        )
+                    }
+
+                    </>
+                ) : (
+                    <div id='inputName' className='row'>
+                    <div className='col-1'>   
+                        <img id='arrow' src={require('../../assets/right-arrow-2.png')} />
+                    </div>
+                    <div className='col-1'>
+                        <img id='tildeIcon' src={require('../../assets/tilde-3.png')} />
+                    </div>
+                    <div id='enterName' className='col-8'>Enter Name:
+                        <input 
+                            type="text" 
+                            id="emailNameField" 
+                            onChange={(e) => setName(e.target.value)}
+                            onKeyDown={handleNameKeyDown}
+                            autoFocus // Focuses on the input field by default
+                        />
+                    </div>
+                    </div>
+                )
+            }
+            </>
+        ) : (
+            <div id='inputEmail' className='row'>
+            <div className='col-1'>   
+                <img id='arrow' src={require('../../assets/right-arrow-2.png')} />
+            </div>
+            <div className='col-1'>
+                <img id='tildeIcon' src={require('../../assets/tilde-3.png')} />
+            </div>
+            <div id='enterEmail' className='col-8'>Enter Email:
+                <input 
+                type="text" 
+                id="emailInputField" 
+                onChange={(e) => setEmail(e.target.value)}
+                onKeyDown={handleKeyDown}
+                autoFocus // Focuses on the input field by default
+                />
+            </div>
+            </div>
+        )}
+
+        </div>
+    </section>
+  )
+}
+
+const BubbleText = () => {
+    return (
+      <h2 className="text-center text-xl font-thin text-indigo-300">
+        {"CourseMap".split("").map((child, idx) => (
+          <span className={styles.hoverText} key={idx}>
+            {child}
+          </span>
+        ))}
+      </h2>
+    );
+  };
+
+function moveCursor(e) {
+    const input = e.target;
+    const cursorPosition = input.selectionStart;
+    const value = input.value;
+  
+    // Move cursor ahead if the length of the email input is not reached
+    if (cursorPosition < value.length) {
+      input.setSelectionRange(cursorPosition + 1, cursorPosition + 1);
+    }
+  }
+
+export default LoginTerminal
---
+++ src/components/navbar/Navbaar.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import "./navbarStyle.css"
+import IconButton from '@mui/material/IconButton';
+import Fingerprint from '@mui/icons-material/Fingerprint';
+
+
+function Navbaar() {
+   
+    return (
+        <div>
+            <Navbar sticky="top" id='navBar' expand="lg" className="">
+                <Container fluid>
+                    <Navbar.Brand href="#">CourseMap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#" disabled>
+                        Courses
+                        </Nav.Link>
+                        <NavDropdown title="Browse" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">
+                            Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Something else here
+                        </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Form className="d-flex">
+                        <button
+                            className={`
+                                px-2.5 py-1 rounded-full 
+                                flex items-center gap-2 
+                                text-slate-500
+                                shadow-[-2px_-2px_5px_rgba(255,_255,_255,_0.8),_2px_2px_5px_rgba(0,_0,_0,_0.25)]
+                                
+                                transition-all
+
+                                hover:shadow-[-1px_-1px_5px_rgba(255,_255,_255,_0.6),_1px_1px_5px_rgba(0,_0,_0,_0.3),inset_-2px_-2px_5px_rgba(255,_255,_255,_1),inset_2px_2px_4px_rgba(0,_0,_0,_0.3)]
+                                hover:text-violet-500
+                            `}
+                            >
+                                <IconButton aria-label="fingerprint" color="secondary">
+                                    <Fingerprint />
+                                </IconButton>
+                                <span style={{marginLeft: "-10px"}}>Sign-Up</span>
+                        </button>
+
+                        <button
+                            id='btnLogin'
+                            className={`
+                                px-2.5 py-1 rounded-full 
+                                flex items-center gap-2 
+                                text-slate-500
+                                shadow-[-2px_-2px_5px_rgba(255,_255,_255,_0.8),_2px_2px_5px_rgba(0,_0,_0,_0.25)]
+                                
+                                transition-all
+
+                                hover:shadow-[-1px_-1px_5px_rgba(255,_255,_255,_0.6),_1px_1px_5px_rgba(0,_0,_0,_0.3),inset_-2px_-2px_5px_rgba(255,_255,_255,_1),inset_2px_2px_4px_rgba(0,_0,_0,_0.3)]
+                                hover:text-green-700
+                            `}
+                            >
+                                <IconButton aria-label="fingerprint" color="success">
+                                    <Fingerprint />
+                                </IconButton>
+                                <span style={{marginLeft: "-10px"}}>Login</span>
+                        </button>
+                    </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+}
+
+
+
+export default Navbaar
